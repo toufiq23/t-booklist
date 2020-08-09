28c4b6c643ed0139b2d5fefaf5feb73416e6c9c6
toufiq23 <drrameraaron023@gmail.com>
2020-08-09 21:39:24 +0200

    done

--- js_oop_booklist.js
+++ js_oop_booklist.js
@@ -123,12 +123,12 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 
 // Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click', (e) => {
-	// Remove book from UI
-	UI.deleteBook(e.target);
+// Remove book from UI
+UI.deleteBook(e.target);
 
-	// Remove book from store
-	Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+// Remove book from store
+Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
-	// Show success message
-	UI.showAlert('Book Removed', 'success');
+// Show success message
+UI.showAlert('Book Removed', 'success');
 });
